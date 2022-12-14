7e07d318862e9f869869bd3b313b7598c8433d47
matiasvargashub <82350780+matiasvargashub@users.noreply.github.com>
2022-12-14 01:40:27 -0300

    Add files via upload

---
+++ js/inicia.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   $('.collapsible').collapsible();
+ });
+ $(document).ready(function(){
+   $('.carousel').carousel();
+ });
+ $(document).ready(function(){
+$('.sidenav').sidenav();
+});
